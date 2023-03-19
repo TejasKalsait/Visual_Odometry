a316209252be10428b28ce1c45366d3c41f5fb9d
TejasKalsait <kalsaittejas10@gmail.com>
2023-03-19 14:03:33 -0400

    Started Code. Setup done.

--- src/lab4_calibration.py
+++ src/lab4_calibration.py
@@ -1,2 +1,16 @@
 #! /usr/bin/env python3
 
+import rospy
+import cv2 as cv
+import numpy
+
+rospy.init_node("calibration_node")
+
+def calibration():
+    # Take images and calibrate
+    pass
+
+if __name__ == '__main__':
+    
+    calibration()
+    rospy.spin()
